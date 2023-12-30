9f842cff5c3d5814ac1bd3676f14035aa0129680
vladislav.bogomolov <vladislav.bogomolov@eltex.loc>
2023-12-30 15:59:14 +0700

    fix bug, enter a width value of status string

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import subprocess
 import time
 import queue
+import platform
 from rich.console import Console
 from rich.table import Table
 
@@ -9,51 +10,71 @@ result_queue = queue.Queue()
 hosts = ['google.com', 'example.com', 'localhost', 'yandex.com', 'habr.com', 'vk.com', 'eltex.loc', '3.3.3.3']
 
 table_structure = {}
+STATUS_WIDTH = 20
 console = Console()
 
+
+def get_operating_system():
+    system = platform.system()
+    if system == 'Windows':
+        return 'Windows'
+    elif system == 'Linux' or system == 'Darwin':
+        return 'Unix'
+    else:
+        return 'Unknown'
+
+
 def ping_cmd(host):
-    result = subprocess.run(['ping', '-c', '1', '-W', '1', host], 
-                            stdout=subprocess.PIPE, 
-                            stderr=subprocess.PIPE, 
-                            text=True)
+    system_name = get_operating_system()
+    if system_name == 'Windows':
+        result = subprocess.run(['ping', '-n', '1', '-w', '1', host],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
+    else:
+        result = subprocess.run(['ping', '-c', '1', '-W', '1', host],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
     result_queue.put({host: [result.returncode, 0, 0]})
+
 
 def queue_to_dict():
     while not result_queue.empty():
         result = result_queue.get()
         for hostname, res in result.items():
+            status = '✓' if res[0] == 0 else 'x'
             if hostname not in table_structure:
-                table_structure[hostname] = '✓' if res[0] == 0 else 'x'
+                table_structure[hostname] = status
             else:
-                if res[0] == 0:
-                    table_structure[hostname] = table_structure[hostname] + '✓'
-                else:
-                    table_structure[hostname] += table_structure[hostname] + 'x'
+                table_structure[hostname] = (table_structure[hostname][-(STATUS_WIDTH - 1):] + status)
         result_queue.task_done()
-        
+
+
 def rich_table():
     console.clear()
     table = Table(title="Hostwatcher")
     table.add_column("Host", style="magenta")
     table.add_column("Status", style="magenta")
+    table.add_column("LEN", style="magenta")
     for key, value in table_structure.items():
-        table.add_row(str(key), str(value))
+        table.add_row(str(key), str(value), str(len(value)))
     console.print(table)
-    
+
 
 def parallel():
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Запускаем пинг для каждого хоста
         results = [executor.submit(ping_cmd, host) for host in hosts]
-        # Получаем результаты выполнения
         for result in concurrent.futures.as_completed(results):
             result.result()
-            
+
+
 def main():
     while True:
         parallel()
         queue_to_dict()
         rich_table()
-    
+
+
 if __name__ == '__main__':
     main()
